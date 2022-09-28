a37eb8b5e42318b717f0390b52ae2b6b109ba00f
eyupGuc <meydanguresi@gmail.com>
2022-09-28 11:48:45 +0300

    added city

--- app.js
+++ app.js
@@ -4,6 +4,31 @@ const fetchWeather = async () => {
   );
   const data = await res.json();
   console.log(data);
+  updateDOM(data);
 };
 
 fetchWeather();
+const updateDOM = (data) => {
+  console.log(data);
+  const rowDiv = document.querySelector(".row");
+  rowDiv.innerHTML += `
+  <div class="card" style="width: 18rem;">
+  <img src="${
+    "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x" + ".png"
+  }" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${data.name}</h5>
+    <p class="card-text"></p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">An item</li>
+    <li class="list-group-item">A second item</li>
+    <li class="list-group-item">A third item</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+  `;
+};
